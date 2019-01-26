d078036d2a13e78bfe4b556d68589e599905b493
ivan-kapelyukh <ivan.kapelyukh@gmail.com>
2019-01-26 20:53:45 +0000

    Pre-parse naughty list

--- nlp/nlp.js
+++ nlp/nlp.js
@@ -1,25 +1,9 @@
-var naughtyList = [];
-fetch('/nlp/block-list-parsed.txt')
-    .then(response => response.text())
-    .then(text => populateNaughtyList(text.split("\n")))
-    .then(main)
-
-function populateNaughtyList(words) {
-    words.map(word => naughtyList.push(word));
-}
-
 function wordIsNaughty(word) {
-    console.log(naughtyList);
-
-    console.log("INPUT TEXT:");
-    var ruskiStr = naughtyList[885];
-    console.log(typeof ruskiStr);
-    for (var i = 0; i < ruskiStr.length; i++) {
-        console.log(ruskiStr.charChodeAt(i));
-    }
-    console.log(naughtyList[885] == "ruski");
+    return naughtyList.includes(word);
 }
 
 function main() {
     console.log(wordIsNaughty("ruski"));
 }
+
+main()
