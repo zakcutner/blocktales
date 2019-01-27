7947399db684e92f5a117435cbb9524b6f1ebe99
Pablo Gamito <pablo@gamito.me>
2019-01-27 13:03:09 +0000

    Updated headless to take peerid as command arg

--- chain/headless.js
+++ chain/headless.js
@@ -4,21 +4,21 @@ const port = 8080;
 
 app.use(express.static('dist'));
 
+const PEERID = process.argv[process.argv.length - 1];
+
 app.listen(port, () => {
   console.log('Listening on port', port);
   const puppeteer = require('puppeteer');
 
-  const MAIN_URL = "http://localhost:8080/#main";
-  
+  const MAIN_URL = "http://localhost:8080/#" + PEERID;
+
   (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     page.on('console', msg => {
       console.log(msg._text);
     });
-  
+
     await page.goto(MAIN_URL);
   })();
 })
-
-
