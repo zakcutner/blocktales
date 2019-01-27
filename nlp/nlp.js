95732e5a0e517dc10f661adc94d1dfe3360ed485
ivan-kapelyukh <ivan.kapelyukh@gmail.com>
2019-01-27 01:56:32 +0000

    Make test results super easy to interpret

--- nlp/nlp.js
+++ nlp/nlp.js
@@ -67,27 +67,32 @@ function passesHolisticHeuristics(sentence) {
 }
 
 // Valid sentence
-validWord("Once upon a time there was a", "hackathon.").then(result => console.log(result));
+validWord("Once upon a time there was a", "hackathon.").then(result => result === true ? console.log("PASS") : console.log("FAIL"));
 // Expect: true
 
 // Repetition
-validWord("Once upon a time there was", "was").then(result => console.log(result));
+validWord("Once upon a time there was", "was").then(result => result === false ? console.log("PASS") : console.log("FAIL"));
 // Expect: false
 
 // Extra whitespace, with repetition
-validWord("Once upon a time there was ", "was").then(result => console.log(result));
+validWord("Once upon a time there was ", "was").then(result => result === false ? console.log("PASS") : console.log("FAIL"));
 // Expect: false
 
 // Sentence terminating with preposition
-validWord("Once upon a time there was", "a.").then(result => console.log(result));
+validWord("Once upon a time there was", "a.").then(result => result === false ? console.log("PASS") : console.log("FAIL"));
 // Expect: false
 
 // Sentence terminating with preposition, separate full stop
-validWord("Once upon a time there was a", ".").then(result => console.log(result));
+validWord("Once upon a time there was a", ".").then(result => result === false ? console.log("PASS") : console.log("FAIL"));
+// Expect: false
+
+// Profanity
+validWord("Once upon a", "ruski").then(result => result === false ? console.log("PASS") : console.log("FAIL"));
 // Expect: false
 
 // Miss-spelling
-validWord("Once upon a", "tiem").then(result => console.log(result));
+validWord("Once upon a", "tiem").then(result => result === false ? console.log("PASS") : console.log("FAIL"));
 // Expect: false
 
 // TODO: can't start sentence with full stop
+// TODO: singular vs plural matching
