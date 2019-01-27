95277cdbcfa55a2a51a8dfc1ed1b51a30b5b0e2f
ivan-kapelyukh <ivan.kapelyukh@gmail.com>
2019-01-27 00:01:34 +0000

    Add heuristics to check for common grammar errors

--- nlp/nlp.js
+++ nlp/nlp.js
@@ -1,8 +1,10 @@
 // use this one!
 async function validWord(sentenceSoFar, word) {
+    sentenceSoFar = sentenceSoFar.split(" ");
     let nice = !wordIsNaughty(word);
     let spelledCorrectly = await wordIsSpelledCorrectly(word);
-    return nice && spelledCorrectly;
+    let heuristicallyCorrect = passesGrammarHeuristics(sentenceSoFar, word);
+    return nice && spelledCorrectly && heuristicallyCorrect;
 }
 
 
@@ -13,6 +15,12 @@ function wordIsNaughty(word) {
 // returns promise
 // idea: if word is spelled correctly, then at least some word will have a similar meaning
 async function wordIsSpelledCorrectly(word) {
+
+    let whitelist = ["."];
+    if (whitelist.includes(word)) {
+        return true;
+    }
+
     let baseURL = "https://api.datamuse.com/words?ml=";
     let suffix = "&max=1";
     let joinedURL = baseURL + word + suffix;
@@ -20,6 +28,7 @@ async function wordIsSpelledCorrectly(word) {
     try {
         let result = await fetch(joinedURL);
         let parsedResult = await result.json();
+        console.log(parsedResult);
         return parsedResult.length != 0;
     }
     catch (err) {
@@ -27,4 +36,22 @@ async function wordIsSpelledCorrectly(word) {
     }
 }
 
-// console.log(validWord("Once upon a time", "imperial").then(result => console.log(result)));
+function passesGrammarHeuristics(sentenceSoFar, newWord) {
+    let lastWord = sentenceSoFar[sentenceSoFar.length - 1];
+    let repeatsWord = lastWord === newWord;
+
+    let wholeSentence = sentenceSoFar.concat([newWord]);
+    let holisticallyCorrect = newWord === "." ? passesHolisticHeuristics(wholeSentence) : true;
+
+    return !repeatsWord && holisticallyCorrect;
+}
+
+function passesHolisticHeuristics(sentence) {
+    let lastWord = sentence[sentence.length - 2]; // very last word is actually "."
+    let prepositions = ["a", "an", "the"];
+    let endsInPreposition = prepositions.includes(lastWord);
+
+    return !endsInPreposition;
+}
+
+// validWord("Once upon a time the", ".").then(result => console.log(result));
